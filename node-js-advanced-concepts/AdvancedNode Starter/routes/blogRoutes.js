21a3c4b9ccd5e613ddccf8952f88b4c7befd1def
Ankit Chaurasia <ac13793@gmail.com>
2018-12-24 23:23:40 +0530

    redis cache implementation

--- node-js-advanced-concepts/AdvancedNode Starter/routes/blogRoutes.js
+++ node-js-advanced-concepts/AdvancedNode Starter/routes/blogRoutes.js
@@ -3,7 +3,7 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
@@ -14,9 +14,23 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisURL = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisURL);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+    // Do we have any cached data in redis related to this query
+    const cachedBlogs = await client.get(req.user.id);
+    // if yes, then response to the request right away and return
+    if (cachedBlogs) {
+      console.log('Serving from cache');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    // else we need to respond to request and update our cache to store the data
     const blogs = await Blog.find({ _user: req.user.id });
-
+    console.log('Serving from mongodb');
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
